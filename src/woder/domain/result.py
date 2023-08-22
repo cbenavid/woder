64dfc13dff7ebc9e0e54615863373876be280541
Carlos Benavides <carlosbenavides35@gmail.com>
2023-08-22 12:21:24 +0200

    feat: define basic Err class

--- src/woder/domain/result.py
+++ src/woder/domain/result.py
@@ -1,6 +1,7 @@
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, Literal, TypeVar
 
 T = TypeVar("T")
+E = TypeVar("E")
 
 
 class Ok(Generic[T]):
@@ -13,3 +14,21 @@ class Ok(Generic[T]):
 
     def __eq__(self, other: Any) -> bool:
         return isinstance(other, Ok) and other.value == self.value
+
+    def __bool__(self) -> Literal[True]:
+        return True
+
+
+class Err(Generic[E]):
+    def __init__(self, value: E, /) -> None:
+        self._value = value
+
+    @property
+    def value(self) -> E:
+        return self._value
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Err) and other.value == self.value
+
+    def __bool__(self) -> Literal[False]:
+        return False
--- tests/domain/test_result.py
+++ tests/domain/test_result.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pytest
 
-from woder.domain.result import Ok
+from woder.domain.result import Err, Ok
 
 
 class TestOk:
@@ -13,3 +13,19 @@ class TestOk:
     def test_can_be_compared(self) -> None:
         assert Ok(None) == Ok(None)
         assert Ok(None) != Ok(1)
+
+    def test_is_truthy(self) -> None:
+        assert bool(Ok(None))
+
+
+class TestErr:
+    @pytest.mark.parametrize("value", [None, 1, "foo"])
+    def test_has_value_attribute(self, value: Any) -> None:
+        assert Err(value).value == value
+
+    def test_can_be_compared(self) -> None:
+        assert Err(None) == Err(None)
+        assert Err(None) != Err(1)
+
+    def test_is_falsy(self) -> None:
+        assert not bool(Err(None))
